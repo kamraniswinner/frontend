f10683542ccdd0cc1836ddbdd94d4f017e1f18a5
kamran <ksf365@gmail.com>
2024-07-16 03:01:03 +0530

    Commit changes in frontend submodule

--- backend/routes/shippingRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { addShipping } from '../controllers/shippingController.js';
-
-const router = express.Router();
-
-router.post('/', addShipping);
-
-export default router;
--- backend/test-import.js
+++
@@ -1,6 +0,0 @@
-// test-import.js
-import path from 'path';
-console.log('Current directory:', path.resolve());
-
-import User from './models/User.js';
-console.log('User model imported:', User);
